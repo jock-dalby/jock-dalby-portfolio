e8a293b0a546f38941d7cfcd102f3a3a46571242
Jock Dalby <jockdalby86@gmail.com>
2023-08-18 12:21:55 +1000

    setup nav links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
 import './index.scss';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Logo from '../../assets/images/logo-avatar.png';
 import LogoName from '../../assets/images/logo-name.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
   return (
@@ -10,6 +12,27 @@ const Sidebar = () => {
         <img src={Logo} alt="logo" />
         <img className="logo-name" src={LogoName} alt="jock" />
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
     </div>
   );
 };
