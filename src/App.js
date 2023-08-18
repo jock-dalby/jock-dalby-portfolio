5c23755f333846ab152954f232f25fccccfe6114
Jock Dalby <jockdalby86@gmail.com>
2023-08-18 14:10:44 +1000

    create and add content to Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
+import Home from './components/Home/Home';
 import Layout from './components/Layout/Layout';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import './Home.scss';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm Jock,
+          <br />
+          web developer.
+        </h1>
+        <h2>Frontend Developer / Javascript Expert</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
