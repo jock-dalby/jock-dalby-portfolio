56c6618d396d830e91ef245b49f863dce32ce663
Jock Dalby <jockdalby86@gmail.com>
2023-08-24 08:23:26 +1000

    fix h1 tags on animated letters on home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,16 +17,18 @@ const Home = () => {
   return (
     <div className="container home-page">
       <div className="text-zone">
-        {STRINGS.map((str, strIdx) => (
-          <span key={strIdx}>
-            <AnimatedLetters
-              delay={4000}
-              str={str}
-              idx={getStartingIdx(strIdx)}
-            />
-            {strIdx !== STRINGS.length - 1 && <br />}
-          </span>
-        ))}
+        <div className="title-letters">
+          {STRINGS.map((str, strIdx) => (
+            <span key={strIdx}>
+              <AnimatedLetters
+                delay={4000}
+                str={str}
+                idx={getStartingIdx(strIdx)}
+              />
+              {strIdx !== STRINGS.length - 1 && <br />}
+            </span>
+          ))}
+        </div>
         <h2>Frontend Developer / Javascript Expert</h2>
         <Link to="/contact" className="flat-button">
           CONTACT ME
