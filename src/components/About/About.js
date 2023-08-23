c7773b1b13ccaf870b1327c1c4f84e049f574cc7
Jock Dalby <jockdalby86@gmail.com>
2023-08-23 10:06:48 +1000

    configure skills cube faces

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,41 @@
 import './About.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons';
+
+const CUBE_FACES = [
+  {
+    icon: faAngular,
+    color: '#DD0031',
+  },
+  {
+    icon: faHtml5,
+    color: '#F06529',
+  },
+  {
+    icon: faCss3,
+    color: '#28A4D9',
+  },
+  {
+    icon: faReact,
+    color: '#5ED4F4',
+  },
+  {
+    icon: faJsSquare,
+    color: '#EFD81D',
+  },
+  {
+    icon: faGitAlt,
+    color: '#EC4D28',
+  },
+];
 
 const About = () => {
   return (
@@ -22,6 +58,17 @@ const About = () => {
           person, father of two beautiful children, a football fanatic, fitness
           enthusiast, and tech-obsessed!!!
         </p>
+      </div>
+      <div className="stage-cube-container">
+        <div className="cube-spinner">
+          {CUBE_FACES.map(({ icon, color }, i) => {
+            return (
+              <div className={`face${i + 1}`}>
+                <FontAwesomeIcon icon={icon} color={color} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
