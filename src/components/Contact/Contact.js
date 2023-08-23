7b8608bff4afab1f0357ac57b7f4ee381c85dc19
Jock Dalby <jockdalby86@gmail.com>
2023-08-24 09:00:48 +1000

    add contact form to Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -15,6 +15,37 @@ const Contact = () => {
             or large projects. However, if you have any other requests or
             questions, don't hesitate to contact me using below form either.
           </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea placeholder="Message" name="message" required />
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
